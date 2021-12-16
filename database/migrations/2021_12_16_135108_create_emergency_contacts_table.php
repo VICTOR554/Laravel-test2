4a3673c40ea7b142d2c5f0628d11c9529cb4d386
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-16 14:01:05 +0000

    created emergency contact model

---
+++ database/migrations/2021_12_16_135108_create_emergency_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmergencyContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emergency_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->bigInteger('animal_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('animal_id')->references('id')->on('animals')
+                ->onDelete('cascade')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emergency_contacts');
+    }
+}
