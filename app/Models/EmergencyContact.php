48f58b3a8ee3c32bef8bc79f9ca7c81a06fcb6c4
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-16 14:04:06 +0000

    Added relations between models

--- app/Models/EmergencyContact.php
+++ app/Models/EmergencyContact.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class EmergencyContact extends Model
 {
     use HasFactory;
+
+    public function animal()
+    {
+        return $this->belongsTo(Animal::class);
+    }
 }
